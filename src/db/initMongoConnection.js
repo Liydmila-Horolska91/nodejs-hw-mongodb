23964b7af743972b4a4e38d6d8a110f0574fdac3
Liydmila <chorolska@gmail.com>
2024-12-02 12:49:30 +0200

    Your commit message

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,8 +1,32 @@
 import mongoose from "mongoose";
 
-const DB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority&appName=MyCluster`;
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ["work", "home", "personal"],
+      required: true,
+      default: "personal",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-export async function initMongoConnection() {
-  await mongoose.connect(DB_URI);
-  console.log("Mongo connection successfully established!");
-}
+export const Contact = mongoose.model("Contact", contactSchema);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,32 +1,8 @@
 import mongoose from "mongoose";
 
-const contactSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-    },
-    isFavourite: {
-      type: Boolean,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      enum: ["work", "home", "personal"],
-      required: true,
-      default: "personal",
-    },
-  },
-  {
-    timestamps: true,
-  },
-);
+const DB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority&appName=MyCluster`;
 
-export const Contact = mongoose.model("Contact", contactSchema);
+export async function initMongoConnection() {
+  await mongoose.connect(DB_URI);
+  console.log("Mongo connection successfully established!");
+}
