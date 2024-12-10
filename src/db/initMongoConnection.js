a11bad7151ae8df7d0163834915e5a564db9ac92
Liydmila <chorolska@gmail.com>
2024-12-10 15:33:47 +0200

    Add CRUD functionality to contacts API

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,42 +1,22 @@
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const DB_URI = process.env.MONGODB_URI;
-
-// console.log('Loaded MongoDB URI:', process.env.MONGODB_URI);
-
-// const initMongoConnection = async () => {
-//   const DB_URI = process.env.MONGODB_URI;
-//   try {
-//     await mongoose.connect(DB_URI);
-//     console.log('MongoDB connected...');
-//   } catch (error) {
-//     console.error('MongoDB connection failed', error);
-//     process.exit(1);
-//   }
-// };
-
-// export default initMongoConnection;
-
-
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const DB_URI = process.env.MONGODB_URI;
 
-const DB_URI = process.env.MONGODB_URL;
-
-console.log('Loaded MongoDB URI:', DB_URI);
+console.log('Loaded MongoDB URI:', DB_URI);  // Перевірка URI в консолі
 
 const initMongoConnection = async () => {
   try {
+    if (!DB_URI) {
+      throw new Error('MongoDB URI is not defined');
+    }
+
     await mongoose.connect(DB_URI);
     console.log('MongoDB connected...');
   } catch (error) {
     console.error('MongoDB connection failed', error);
-    process.exit(1);
+    process.exit(1);  // Завершення процесу з помилкою
   }
 };
 
