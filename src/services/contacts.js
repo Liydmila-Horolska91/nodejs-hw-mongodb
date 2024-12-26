45106daf42d1fe9073b169061edb7f403f9bee6f
Liydmila <chorolska@gmail.com>
2024-12-26 21:22:06 +0200

    Add CRUD functionality to contacts API

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,12 +1,7 @@
 import { SORT_ORDER } from '../constans/index.js';
-import Contact from '../models/contact.js';
+import Contact from '../models/contacts.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-// const clearFilter = (filter) => {
-//   return Object.fromEntries(
-//     Object.entries(filter).filter(([_, value]) => value !== undefined),
-//   );
-// };
 
 const getAllContacts = async ({
   page = 1,
